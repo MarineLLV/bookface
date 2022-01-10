e376d9f66e365c1ccfd38371cd732f4086a63fad
MarineLLV <marine.lucottelevisage@gmail.com>
2022-01-10 16:41:26 +0100

    add Head tags

---
+++ components/Layout/HeadTags.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+function HeadTags() {
+    return (
+        <>
+            <Head>
+                <meta charset="UTF-8" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+                <title>BookFace media</title>
+            </Head>
+        </>
+    );
+}
+
+export default HeadTags;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
+//import App from 'next/app' // Custom App
 import '../styles/globals.css'
+
+/* class MyApp extends App {
+  render() {
+    const { Component } = this.props;
+
+    return (
+      <Component />
+    );
+  }
+} */
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+// export default MyApp;
