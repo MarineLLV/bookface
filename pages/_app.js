f478711ae6793674775d9b2de6df7a652acc75be
MarineLLV <marine.lucottelevisage@gmail.com>
2022-01-16 11:59:34 +0100

    add getInitialProps for server-side rendering

--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,23 @@ import theme from "../components/ui/Theme";
 
 
 function MyApp({ Component, pageProps }) {
-  return <ThemeProvider theme={theme}><Component {...pageProps} /></ThemeProvider>
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+// GetInitialprops = way to get server side rendering in the app
+MyApp.getInitialProps = async (Component, ctx) => {
+  let pageProps = {};
+
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  return { pageProps };
+
 }
 
 export default MyApp;
